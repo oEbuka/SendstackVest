27999fa7ce211c8b82ced9158a7fdd4eea31eb98
Ebuka <ebukaobiora10@gmail.com>
2023-11-16 12:59:13 +0100

    fixed bug

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const TransactionController = require('./Controllers/TransactionController');
-
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
